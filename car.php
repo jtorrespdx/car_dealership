41ee447cfdf2e00dc8674f563a294ed9b994c2bc
Samantha Maturen & Jose Torres <student@epicodus.com>
2015-08-03 14:26:56 -0700

    Added constructor object

--- car.php
+++ car.php
@@ -5,31 +5,46 @@
         public $price;
         public $miles;
 
+        function __construct($car_make, $car_price, $car_miles)
+        {
+            $this->make_model = $car_make;
+            $this->price = $car_price;
+            $this->miles = $car_miles;
+        }
+
         function worthBuying($max_price)
         {
             return $this->price < ($max_price + 100);
         }
     }
-
+    /*
     $porsche = new Car();
     $porsche->make_model = "2014 Porsche 911";
     $porsche->price = 114991;
     $porsche->miles = 7864;
-
+    */
+    $porsche = new Car("2014 Porsche 911", 114991, 7861);
+    /*
     $ford = new Car();
     $ford->make_model = "2011 Ford F450";
     $ford->price = 55995;
     $ford->miles = 14241;
-
+    */
+    $ford = new Car("2011 Ford F450", 55995, 14241);
+    /*
     $lexus = new Car();
     $lexus->make_model = "2013 Lexus RX 350";
     $lexus->price = 44700;
     $lexus->miles = 20000;
-
+    */
+    $lexus = new Car("2013 Lexus RX 350", 44700, 20000);
+    /*
     $mercedes = new Car();
     $mercedes->make_model = "Mercedes Benz CLS550";
     $mercedes->price = 39900;
     $mercedes->miles = 37979;
+    */
+    $mercedes = new Car("Mercedes Benz CLS550", 39900, 37979);
 
     $cars = array($porsche, $ford, $lexus, $mercedes);
 
